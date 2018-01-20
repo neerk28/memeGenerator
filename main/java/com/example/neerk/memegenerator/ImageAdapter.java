170c22a2848382831ec7e891f770a0e3f72088c3
neerk28 <34616108+neerk28@users.noreply.github.com>
2018-01-21 00:43:12 +0530

    Ability to add two pics +editable font size

---
+++ main/java/com/example/neerk/memegenerator/ImageAdapter.java
@@ -0,0 +1,57 @@
+package com.example.neerk.memegenerator;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+/**
+ * Created by neerk on 10/01/18.
+ */
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    @Override
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup viewGroup) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[i]);
+        return imageView;
+    }
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.success_kid, R.drawable.stare_animal
+    };
+
+}
